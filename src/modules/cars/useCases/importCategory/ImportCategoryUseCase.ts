6ed078bdb2e1bec92ba4912711a0c2d6a92388c6
yancaa <yan.cesar@sistemavitto.com.br>
2021-08-09 12:30:23 -0300

    feat:Iplementando importação de category e o swagger

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,18 +3,54 @@ import fs from 'fs';
 
 import { ICategoriesReposity } from '../../repositories/ICategoriesReposity';
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
   constructor(private categoryRepository: ICategoriesReposity) {}
 
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
 
-    const parseFile = csvParse();
+      const parseFile = csvParse();
 
-    stream.pipe(parseFile);
+      stream.pipe(parseFile);
 
-    parseFile.on('data', async line => {
-      console.log(line);
+      parseFile
+        .on('data', async line => {
+          const [name, description] = line;
+
+          categories.push({ name, description });
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on('error', err => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
+
+    categories.map(async category => {
+      const { name, description } = category;
+
+      const existCategory = this.categoryRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoryRepository.create({
+          name,
+          description,
+        });
+      }
     });
   }
 }
